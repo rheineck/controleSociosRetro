2e42729edd8bcc4fcfa3825a4b4d545981f2195c
Raphael <raphaelheineck@gmail.com>
2024-08-19 00:04:18 +0000

    web: changes in style, initiated implementations of theme

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ web/src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    GRAY_900: "#121214",
+    GRAY_400: "#7C7C8A",
+    GRAY_800: "#202024",
+    GRAY_100: "#E1E1E6",
+    GRAY_200: "#C4C4CC",
+
+    RED: "#E21B1B",
+    BLACK: "#000000"
+
+  }
+}
