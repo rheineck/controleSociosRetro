309455690a4f2d21a097c30652eba9ac32c8eb70
Raphael <raphaelheineck@gmail.com>
2024-08-28 08:27:11 -0300

    server: database initiated

---
+++ server/src/database/knex/migrations/20240827203157_createUsers.js
@@ -0,0 +1,6 @@
+exports.up = knex => knex.schema.createTable("users", table => {
+  table.increments('id');
+  table.text('name');
+  table.text('email');
+  table.text('maritalStatus');
+})
---
+++ server/src/index.js
@@ -0,0 +1,31 @@
+require("express-async-errors");
+require("dotenv/config");
+
+const AppError = require("./utils/AppError");
+
+const cors = require("cors");
+const express = require("express");
+// const routes = require("./routes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+})
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
